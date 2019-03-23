adcc7074df688a5b91bdf7a6b25099f6dc7d5242
Charutito <mf.milewski@gmail.com>
2019-03-23 00:18:46 -0300

    [FIRST COMMIT] Upload the entire project

---
+++ Assets/Resourses/Scripts/CharacterMovement.cs
@@ -0,0 +1,25 @@
+﻿using Managers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        if (InputManager.Instance.AxisMoving)
+        {
+            MoveTransform(InputManager.Instance.AxisHorizontal, InputManager.Instance.AxisVertical);
+        }
+    }
+
+    public void MoveTransform(float x, float z)
+    {
+        var newPosition = transform.position + (new Vector3(x, 0, z) * speed * Time.deltaTime);
+        
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/Resourses/Scripts/InputManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using GameUtils;
+using UnityEngine;
+
+
+namespace Managers
+{
+    public class InputManager : SingletonObject<InputManager>
+    {
+        #region Properties
+
+        public bool AxisMoving { get { return (Mathf.Abs(AxisHorizontal) + Mathf.Abs(AxisVertical)) > 0; } }
+
+        public float AxisHorizontal { get { return Input.GetAxis("Horizontal"); } }
+        public float AxisVertical { get { return Input.GetAxis("Vertical"); } }
+
+        #endregion
+    }
+}
