4a4141885eb5e9220964a54fc3149496a312f202
Charutito <mf.milewski@gmail.com>
2019-03-27 00:22:10 -0300

    [CHARACTER] Character movement + art changes

--- Assets/Resourses/Scripts/CharacterMovement.cs
+++ Assets/Resourses/Scripts/CharacterMovement.cs
@@ -7,19 +7,41 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public float speed;
+    public GameObject shadow;
+
+    private Vector3 characterScale;
+    private float characterScaleX;
+
+    void Start()
+    {
+        characterScale = transform.localScale;
+        characterScaleX = characterScale.x;
+    }
 
     private void FixedUpdate()
     {
         if (InputManager.Instance.AxisMoving)
         {
             MoveTransform(InputManager.Instance.AxisHorizontal, InputManager.Instance.AxisVertical);
+            FlipPosition(InputManager.Instance.AxisHorizontal);
         }
     }
 
-    public void MoveTransform(float x, float z)
+    private void FlipPosition(float xHorizontal)
     {
-        var newPosition = transform.position + (new Vector3(x, 0, z) * speed * Time.deltaTime);
-        
-        transform.position = newPosition;
+        if (xHorizontal < 0){
+            characterScale.x = -characterScaleX;
+        }
+        if (xHorizontal > 0)        {
+            characterScale.x = characterScaleX;
+        }
+
+        transform.localScale = characterScale;
+    }
+
+    public void MoveTransform(float x, float z)
+    {  
+        var newPosition = transform.position + (new Vector3(x, 0, z) * speed * Time.deltaTime);        
+        transform.position = newPosition;       
     }
 }
--- Assets/Resourses/Scripts/InputManager.cs
+++ Assets/Resourses/Scripts/InputManager.cs
@@ -11,8 +11,8 @@ namespace Managers
 
         public bool AxisMoving { get { return (Mathf.Abs(AxisHorizontal) + Mathf.Abs(AxisVertical)) > 0; } }
 
-        public float AxisHorizontal { get { return Input.GetAxis("Horizontal"); } }
-        public float AxisVertical { get { return Input.GetAxis("Vertical"); } }
+        public float AxisHorizontal { get { return Input.GetAxisRaw("Horizontal"); } }
+        public float AxisVertical { get { return Input.GetAxisRaw("Vertical"); } }
 
         #endregion
     }
